858214b7fc10179124addf51512fffe156eeb655
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-02 18:30:19 -0500

    add defer-option component

---
+++ src/app/dashboard/pages/defer-option/defer-option.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './defer-option.component.html',
+  styles: ``
+})
+export class DeferOptionComponent {
+
+}
