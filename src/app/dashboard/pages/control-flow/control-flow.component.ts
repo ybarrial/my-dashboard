588213b97ec7601e84f6d1969565b291ee013006
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-05 15:47:33 -0500

    new way to use switch()   @switch(){}

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'F';
+
 @Component({
   standalone: true,
   imports: [],
@@ -9,6 +11,7 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
 
   public toggleContent() {
