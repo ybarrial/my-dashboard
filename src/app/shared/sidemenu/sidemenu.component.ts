374a4ff2897f9463dc60ecd2ff822136549f30e9
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-05 12:05:31 -0500

    add imports routerModule & commonModule

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
