dfab75d7173117fa104a174af6509286120975fe
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-06 15:55:27 -0500

    change Detection OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+    /******    *******/
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  /*************/
+  template: `
+  
+    <app-title [title]="currentFramework()" />
+
+  <pre> {{ frameworkAsSignal() | json }} </pre>
+  <pre> {{ frameworkAsProperty | json }} </pre>
+
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change Detection - ${ this.frameworkAsSignal().name } `
+  )
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  })
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+
+      //this.frameworkAsProperty.name = 'React';
+
+      // this.frameworkAsSignal.update( value => {
+      //   value.name = 'React';
+      //   return {...value};
+      // })
+
+      this.frameworkAsSignal.update( value => ({
+        ...value,
+        name: 'React'
+      }))
+
+      console.log('Hecho');
+    }, 3000);
+
+  }
+
 
 }
