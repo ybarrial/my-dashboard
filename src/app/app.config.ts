178b6120c112d461d3bae789dcd47a0580602d61
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-07 14:51:16 -0500

    add in main provider the transition Initial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        // saltar la transicion al cargar la pagina por primera vez.
+        skipInitialTransition: true,
+        // Informacion de la transicion
+        // onViewTransitionCreated( transitionInfo ) {
+        //   console.log({ transitionInfo });
+        // }
+      }),
+    ),
+  ]
 };
