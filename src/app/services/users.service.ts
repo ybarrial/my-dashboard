627dd8f0abeb0f6548e9dd59ef554500a06a86cd
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-07 22:44:21 -0500

    add computed signals

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,28 +1,39 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
 
-import { User } from '@interfaces/req-response';
-
+import { User, UsersResponse } from '@interfaces/req-response';
+import { delay } from 'rxjs';
 
 interface State {
   users: User[];
   loading: boolean;
 }
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+
+  private http = inject( HttpClient );
 
   #state = signal<State>({
     loading: true,
     users: [],
   });
 
+  public users = computed( () => this.#state().users );
+  public loading = computed( () => this.#state().loading );
+
+
   constructor() {
 
-
-    console.log('Cargando data');
-
+    this.http.get<UsersResponse>( 'https://reqres.in/api/users' )
+      .pipe( delay(1500) )
+      .subscribe( res => {
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        })
+      });    
   }
 }
