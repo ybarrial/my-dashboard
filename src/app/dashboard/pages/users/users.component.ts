3a3d8ddc2b8339044899d502885b062f9e35bfe6
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-07 22:38:43 -0500

    add imports

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, TitleComponent],
   templateUrl: './users.component.html',
   styles: ``
 })
