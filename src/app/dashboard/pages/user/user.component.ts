080a59641e8eafe2fc2fb8b0bb8536b3363c0132
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-08 00:13:08 -0500

    computed signal

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '@interfaces/req-response';
 import { UsersService } from '@services/users.service';
@@ -12,7 +12,7 @@ import { switchMap } from 'rxjs';
   standalone: true,
   imports: [CommonModule, TitleComponent],
   template: `
-    <app-title title="User" />
+    <app-title [title]="titleLabel()" />
 
     @if ( user() ) {
       <section>
@@ -38,14 +38,21 @@ import { switchMap } from 'rxjs';
 export default class UserComponent {
 
   private route = inject( ActivatedRoute );
-  private usersService = inject( UsersService )
+  private usersService = inject( UsersService );
 
-  // public user = signal<User | undefined>(undefined);
-  public user = toSignal(
-    this.route.params.pipe(
-      switchMap( ({id}) => this.usersService.getUserById( id ) )
+    // public user = signal<User | undefined>(undefined);
+    public user = toSignal(
+      this.route.params.pipe(
+        switchMap( ({id}) => this.usersService.getUserById( id ) )
+      )
     )
-  )
+
+  public titleLabel = computed( () => {
+    if( this.user() ){
+      return `Informacion del usuario: ${ this.user()?.first_name } ${ this.user()?.last_name } `
+    }
+    return 'Informacion del usuario';
+  })
 
   // constructor() {
   //   this.route.params.subscribe( params => {
