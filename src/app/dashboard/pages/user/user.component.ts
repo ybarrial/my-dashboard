de56bdb4f7d90327e1cb784d54159b9a97f7f77f
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-08 00:04:25 -0500

    Observable to Signal  toSignal()

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '@interfaces/req-response';
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
+
+import { toSignal } from '@angular/core/rxjs-interop'
+import { switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="User" />
+
+    @if ( user() ) {
+      <section>
+
+        <img
+          [srcset]="user()!.avatar"
+          [alt]="user()!.first_name"
+        />
+
+        <div>
+          <h3>{{ user()?.first_name }} {{ user()?.last_name }}</h3>
+          <p>{{ user()?.email }}</p>
+        </div>
+
+      </section>
+
+    } @else {
+      <p>Cargando informacion</p>
+    }
+
+  `,
 })
 export default class UserComponent {
+
+  private route = inject( ActivatedRoute );
+  private usersService = inject( UsersService )
+
+  // public user = signal<User | undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({id}) => this.usersService.getUserById( id ) )
+    )
+  )
+
+  // constructor() {
+  //   this.route.params.subscribe( params => {
+  //     console.log({ params });
+  //   })
+  // }
 
 }
