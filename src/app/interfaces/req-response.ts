306a29831b487eaecd2a7bac9f5f0b994f868829
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-08 00:01:58 -0500

    add UserResponse to interface

--- src/app/interfaces/req-response.ts
+++ src/app/interfaces/req-response.ts
@@ -9,6 +9,13 @@ export interface UsersResponse {
     support:     Support;
 }
 
+
+export interface UserResponse {
+    data: User;
+    support: Support;
+}
+
+
 export interface User {
     id:         number;
     email:      string;
